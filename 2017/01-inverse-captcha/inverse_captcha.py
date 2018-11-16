69f2a8c8cb444ea11aa64484f715e6987d8923cb
Christian Guggenmos <Christian.Guggenmos@bmw.de>
2018-11-16 10:10:11 +0100

    2017-01: Python solution part 2 (comprehension and loop)

--- 2017/01-inverse-captcha/inverse_captcha.py
+++ 2017/01-inverse-captcha/inverse_captcha.py
@@ -11,28 +11,30 @@ def int_to_digit_list(number):
     return list(map(int, str(number)))
 
 
-def calc_checksum_comprehension(input):
+def calc_checksum_comprehension(input, neighbor_dist = 1):
     count = 0
     if not len(str(input)) < 2:
         input_list = int_to_digit_list(input)
-        count = sum(n for idx, n in enumerate(input_list) if n == input_list[idx-1])
+        count = sum(n for idx, n in enumerate(input_list) if n == input_list[idx - neighbor_dist])
     return count
 
 
-def calc_checksum_loop(input):
+def calc_checksum_loop(input, neighbor_dist = 1):
     count = 0
     if not len(str(input)) < 2:
         input_list = int_to_digit_list(input)
         for idx, number in enumerate(input_list):
-            if number == input_list[idx-1]:
+            if number == input_list[idx - neighbor_dist]:
                 count += number
     return count
 
 
 def main():
     input_data = read_first_line('inverse_captcha_input')
-    print(f'Checksum for input (comprehension): {calc_checksum_comprehension(input_data)}')
-    print(f'Checksum for input (loop): {calc_checksum_loop(input_data)}')
+    print(f'Checksum for input (comprehension, neighbor distance = 1): {calc_checksum_comprehension(input_data)}')
+    print(f'Checksum for input (loop, neighbor distance = 1): {calc_checksum_loop(input_data)}')
+    print(f'Checksum for input (loop, neighbor distance = half input size): {calc_checksum_comprehension(input_data, len(input_data)//2)}')
+    print(f'Checksum for input (loop, neighbor distance = half input size): {calc_checksum_loop(input_data, len(input_data)//2)}')
 
 
 if __name__ == '__main__':
--- 2017/01-inverse-captcha/test_inverse_captcha.py
+++ 2017/01-inverse-captcha/test_inverse_captcha.py
@@ -4,19 +4,34 @@ import unittest, inverse_captcha
 class TestWordProcessor(unittest.TestCase):
 
     def test_calc_checksum_comprehension(self):
+        # neighbor distance 1
         self.assertEqual(inverse_captcha.calc_checksum_comprehension(1), 0)
         self.assertEqual(inverse_captcha.calc_checksum_comprehension(1122), 3)
         self.assertEqual(inverse_captcha.calc_checksum_comprehension(1111), 4)
         self.assertEqual(inverse_captcha.calc_checksum_comprehension(1234), 0)
         self.assertEqual(inverse_captcha.calc_checksum_comprehension(91212129), 9)
+        # neighbor distance half input size
+        self.assertEqual(inverse_captcha.calc_checksum_comprehension(1212, 2), 6)
+        self.assertEqual(inverse_captcha.calc_checksum_comprehension(1221, 2), 0)
+        self.assertEqual(inverse_captcha.calc_checksum_comprehension(123425, 3), 4)
+        self.assertEqual(inverse_captcha.calc_checksum_comprehension(123123, 3), 12)
+        self.assertEqual(inverse_captcha.calc_checksum_comprehension(12131415, 4), 4)
+
 
 
     def test_calc_checksum_loop(self):
+        # neighbor distance 1
         self.assertEqual(inverse_captcha.calc_checksum_loop(1), 0)
         self.assertEqual(inverse_captcha.calc_checksum_loop(1122), 3)
         self.assertEqual(inverse_captcha.calc_checksum_loop(1111), 4)
         self.assertEqual(inverse_captcha.calc_checksum_loop(1234), 0)
         self.assertEqual(inverse_captcha.calc_checksum_loop(91212129), 9)
+        # neighbor distance half input size
+        self.assertEqual(inverse_captcha.calc_checksum_loop(1212, 2), 6)
+        self.assertEqual(inverse_captcha.calc_checksum_loop(1221, 2), 0)
+        self.assertEqual(inverse_captcha.calc_checksum_loop(123425, 3), 4)
+        self.assertEqual(inverse_captcha.calc_checksum_loop(123123, 3), 12)
+        self.assertEqual(inverse_captcha.calc_checksum_loop(12131415, 4), 4)
 
 
 if __name__ == '__main__':
