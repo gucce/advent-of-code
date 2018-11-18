43155ea3886505be08c1301fc14cb0feb493be35
Christian Guggenmos <Christian.Guggenmos@bmw.de>
2018-11-18 21:14:18 +0100

    2017-05: Python solution part 2

--- 2017/05-jump-list/jump_list.py
+++ 2017/05-jump-list/jump_list.py
@@ -6,7 +6,14 @@ def read_file(file_path):
         return f.read()
 
 
-def jump(instructions):
+def increment_val(value, advanced = False):
+    if advanced and value >= 3:
+        return value - 1
+    else:
+        return value + 1
+
+
+def jump(instructions, advanced = False):
     steps, current_idx = 0, 0
     while True:
         steps += 1
@@ -14,13 +21,14 @@ def jump(instructions):
         if next_idx >= len(instructions):
             print(f'return current_idx {current_idx}, next_idx: {next_idx}')
             return steps
-        instructions[current_idx] += 1
+        instructions[current_idx] = increment_val(instructions[current_idx], advanced)
         current_idx = next_idx
 
 
 def main():
     input = read_file('jump_list_input')
-    print(jump([int(n) for n in input.splitlines()]))
+    print(jump([int(n) for n in input.splitlines()], advanced = False))
+    print(jump([int(n) for n in input.splitlines()], advanced = True))
 
 
 if __name__ == '__main__':
--- 2017/05-jump-list/test_jump_list.py
+++ 2017/05-jump-list/test_jump_list.py
@@ -6,7 +6,8 @@ import jump_list
 class TestWordProcessor(unittest.TestCase):
 
     def test_count_valid_lines_duplicates(self):
-        self.assertEqual(jump_list.jump([int(n) for n in '0 3 0 1 -3'.split()]), 5)
+        self.assertEqual(jump_list.jump([int(n) for n in '0 3 0 1 -3'.split()], advanced = False), 5)
+        self.assertEqual(jump_list.jump([int(n) for n in '0 3 0 1 -3'.split()], advanced = True), 10)
 
 
 if __name__ == '__main__':
