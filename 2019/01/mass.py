efcdc0cc99473afab6b4605a0e156ab339e63125
Christian Guggenmos <Christian.Guggenmos@bmw.de>
2019-12-01 17:46:41 +0100

    2019-01 part 2

--- 2019/01/mass.py
+++ 2019/01/mass.py
@@ -1,27 +1,32 @@
-from collections import namedtuple
-import itertools
-
-Step = namedtuple('Step', 'direction_change steps')
-Point = namedtuple('Point', 'direction x y')
-
-
 def read_file(file_path):
     with open(file_path, 'r', encoding="UTF-8") as f:
         return f.read()
+
+
+def calc_fuel_for_mass_recursive(mass: int):
+    fuel = calc_fuel_for_mass(mass)
+    if fuel <= 0:
+        return 0
+    else:
+        return fuel + calc_fuel_for_mass_recursive(fuel)
 
 
 def calc_fuel_for_mass(mass: int):
     return int(mass/3) - 2
 
 
-def calc_fuel_for_modules(module_masses):
-    return sum(map(calc_fuel_for_mass, module_masses))
+def calc_fuel_for_modules(module_masses, fuel_calculation):
+    return sum(map(fuel_calculation, module_masses))
+
+
+def map_input(input_):
+    return list(map(int, input_.splitlines()))
 
 
 def main():
-    input_ = read_file('input')
-    module_masses = map(int, input_.splitlines())
-    print(calc_fuel_for_modules(module_masses))
+    module_masses = map_input(read_file('input'))
+    print(calc_fuel_for_modules(module_masses, calc_fuel_for_mass))
+    print(calc_fuel_for_modules(module_masses, calc_fuel_for_mass_recursive))
 
 
 if __name__ == '__main__':
--- 2019/01/test_mass.py
+++ 2019/01/test_mass.py
@@ -14,11 +14,17 @@ class TestWalk(unittest.TestCase):
         self.assertEqual(654, mass.calc_fuel_for_mass(1969))
         self.assertEqual(33583, mass.calc_fuel_for_mass(100756))
 
-    # def test_part1(self):
-    #     self.assertEquals(253, mass.walk_steps(mass.map_input(self.input_)))
-    #
-    # def test_part2(self):
-    #     self.assertEquals(126, mass.walk_steps(mass.map_input(self.input_, single_steps=True), search_visited_twice=True))
+    def test_calc_mass_recursive(self):
+        self.assertEqual(2, mass.calc_fuel_for_mass_recursive(12))
+        self.assertEqual(966, mass.calc_fuel_for_mass_recursive(1969))
+        self.assertEqual(50346, mass.calc_fuel_for_mass_recursive(100756))
+
+    def test_part1(self):
+        self.assertEquals(3173518, mass.calc_fuel_for_modules(mass.map_input(self.input_), mass.calc_fuel_for_mass))
+
+    def test_part2(self):
+        self.assertEquals(4757427,
+                          mass.calc_fuel_for_modules(mass.map_input(self.input_), mass.calc_fuel_for_mass_recursive))
 
 
 if __name__ == '__main__':
