cfd2eceb24c479f6b31440322c694303b002432c
Christian Guggenmos <Christian.Guggenmos@bmw.de>
2020-12-01 14:49:14 +0100

    2020-01 Python part 2

--- 2020/01-python/find_sum.py
+++ 2020/01-python/find_sum.py
@@ -11,15 +11,20 @@ def map_input(input_):
     return list(map(int, input_))
 
 
-def part1(digits):
-    for c in comb(digits, 2):
+def part1(digits, comb_len=2):
+    for c in comb(digits, comb_len):
         if sum(c) == 2020:
             return math.prod(c)
+
+
+def part2(digits):
+    return part1(digits, comb_len=3)
 
 
 def main():
     digits = map_input(read_file('input'))
     print('Part 1: ', part1(digits))
+    print('Part 2: ', part2(digits))
 
 
 if __name__ == '__main__':
--- 2020/01-python/test_sum.py
+++ 2020/01-python/test_sum.py
@@ -4,9 +4,15 @@ import find_sum
 
 class TestLayers(unittest.TestCase):
 
+    def setUp(self) -> None:
+        super().setUp()
+        self.input_ = [1721, 979, 366, 299, 675, 1456]
+
     def test_part1(self):
-        input_ = [1721, 979, 366, 299, 675, 1456]
-        self.assertEqual(514579, find_sum.part1(input_))
+        self.assertEqual(514579, find_sum.part1(self.input_))
+
+    def test_part2(self):
+        self.assertEqual(241861950, find_sum.part2(self.input_))
 
 
 if __name__ == '__main__':
